129bfb9fab017d45524d1cd6a1ac86d2be4b9c48
Sam Cook <email@sjc.sh>
2024-03-13 21:08:33 -0500

    tweak waitForAppReadiness

--- internal/dev/wait_for_app_readiness.go
+++ internal/dev/wait_for_app_readiness.go
@@ -10,18 +10,19 @@ import (
 func waitForAppReadiness(config *common.Config) bool {
 	maxAttempts := config.DevConfig.MaxReadinessAttempts
 	if maxAttempts == 0 {
-		maxAttempts = 30
+		maxAttempts = 100
 	}
 	readinessSleepTime := config.DevConfig.ReadinessSleepTime
 	if readinessSleepTime == 0 {
-		readinessSleepTime = 300 * time.Millisecond
+		readinessSleepTime = 20 * time.Millisecond
 	}
 	for attempts := 0; attempts < maxAttempts; attempts++ {
 		resp, err := http.Get(config.DevConfig.HealthcheckURL)
 		if err == nil && resp.StatusCode == http.StatusOK {
 			return true
 		}
-		time.Sleep(readinessSleepTime)
+		additionalDelay := time.Duration(attempts * 20)
+		time.Sleep(readinessSleepTime + additionalDelay*time.Millisecond)
 	}
 	return false
 }
